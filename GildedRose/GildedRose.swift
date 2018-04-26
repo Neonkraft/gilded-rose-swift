b3686af6fb4850a09e244cf926fa8f0315d6332a
Krishnakumar, Arjun <arjun.krishnakumar@sap.com>
2018-04-26 14:20:39 +0530

    Improved code readability

--- GildedRose/GildedRose.swift
+++ GildedRose/GildedRose.swift
@@ -7,60 +7,121 @@
 //
 
 public class GildedRose {
-
+    
+    static let legendaryItems = ["Sulfuras, Hand of Ragnaros"]
+    static let deadlineItems = ["Backstage passes to a TAFKAL80ETC concert"]
+    static let timelessItems = ["Aged Brie"]
+    
+    public static func isLegendaryItem(_ item: String) -> Bool{
+        return legendaryItems.contains(item)
+    }
+    
+    public static func isItemWithDeadline(_ item: String) -> Bool {
+        return deadlineItems.contains(item)
+    }
+    
+    public static func isItemWhichImprovesWithTime(_ item: String) -> Bool {
+        return timelessItems.contains(item)
+    }
+    
+    public static func isOrdinaryItem(_ item: String) -> Bool {
+        return (!isLegendaryItem(item) && !isItemWithDeadline(item) && !isItemWhichImprovesWithTime(item))
+    }
+    
+    public static func isConjuredItem(_ item: String) -> Bool {
+        return item.hasPrefix("Conjured")
+    }
+    
     public static func updateQuality(items: Array<Item>) -> [Item] {
-        var items = items
-        for i in 0 ..< items.count {
-            if items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert" {
-                if items[i].quality > 0 {
-                    if items[i].name != "Sulfuras, Hand of Ragnaros" {
-                        items[i].quality = items[i].quality - 1
-                    }
-                }
-            } else {
-                if items[i].quality < 50 {
-                    items[i].quality = items[i].quality + 1
-                    
-                    if items[i].name == "Backstage passes to a TAFKAL80ETC concert" {
-                        if items[i].sellIn < 11 {
-                            if items[i].quality < 50 {
-                                items[i].quality = items[i].quality + 1
-                            }
-                        }
-                        
-                        if items[i].sellIn < 6 {
-                            if items[i].quality < 50 {
-                                items[i].quality = items[i].quality + 1
-                            }
-                        }
-                    }
-                }
+        var updatedItems: [Item] = []
+        var index = 0
+        
+        for item in items {
+            var updatedItem: Item?
+            if isOrdinaryItem(item.name) {
+                updatedItem = decreaseQuality(ofItem: item, withThreshold: 0)
+            } else if isItemWithDeadline(item.name) {
+                updatedItem = decreaseQualityForDeadlineItem(item)
+            } else if isItemWhichImprovesWithTime(item.name) {
+                updatedItem = increaseQuality(ofItem: item, withThreshold: 50)
+            } else if isConjuredItem(item.name) {
+                updatedItem = decreaseQuality(ofItem: item, withThreshold: 0, byValue: 2)
+            } else if isLegendaryItem(item.name) {
+                updatedItem = item
             }
             
-            if items[i].name != "Sulfuras, Hand of Ragnaros" {
-                items[i].sellIn = items[i].sellIn - 1
-            }
-            
-            if items[i].sellIn < 0 {
-                if items[i].name != "Aged Brie" {
-                    if items[i].name != "Backstage passes to a TAFKAL80ETC concert" {
-                        if items[i].quality > 0 {
-                            if items[i].name != "Sulfuras, Hand of Ragnaros" {
-                                items[i].quality = items[i].quality - 1
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality
-                    }
-                } else {
-                    if items[i].quality < 50 {
-                        items[i].quality = items[i].quality + 1
-                    }
+            if var updateItem = updatedItem {
+                if isLegendaryItem(updateItem.name) == false {
+                    updateItem.sellIn = updateItem.sellIn - 1
                 }
+                updatedItems.append(updateItem)
+                index = index + 1
             }
         }
         
-        return items
+        return updatedItems
     }
     
+    private static func decreaseQuality(ofItem item: Item, withThreshold threshold: Int, byValue: Int) -> Item {
+        var item = item
+        for _ in 0 ..< byValue {
+            item = decreaseQuality(ofItem: item, withThreshold: threshold)
+        }
+        
+        return item
+    }
+    
+    private static func decreaseQuality(ofItem item: Item, withThreshold threshold: Int)  -> Item {
+        var item = item
+        var decreaseVal = 1
+        
+        if item.sellIn <= 0 {
+            decreaseVal = 2
+        }
+        
+        if item.quality - decreaseVal >= threshold {
+            item.quality = item.quality - decreaseVal
+        } else {
+            item.quality = 0
+        }
+        
+        return item
+    }
+    
+    private static func increaseQuality(ofItem item: Item, withThreshold threshold: Int) -> Item {
+        var item = item
+        var increaseVal = 1
+        
+        if item.sellIn <= 0 {
+            increaseVal = 2
+        }
+        
+        if item.quality + increaseVal <= threshold {
+            item.quality = item.quality + increaseVal
+        } else {
+            item.quality = threshold
+        }
+        
+        return item
+    }
+    
+    private static func decreaseSellIn(ofItem item: Item) -> Item {
+        var item = item
+        item.sellIn = item.sellIn - 1
+        return item
+    }
+    
+    private static func decreaseQualityForDeadlineItem(_ item: Item) -> Item {
+        var item = item
+        
+        if item.sellIn <= 0 {
+            item.quality = 0
+        } else if item.sellIn <= 5 {
+            item = decreaseQuality(ofItem: item, withThreshold: 0, byValue: 2)
+        } else if item.sellIn <= 10 {
+            item = decreaseQuality(ofItem: item, withThreshold: 0, byValue: 5)
+        }
+        
+        return item
+    }
 }
